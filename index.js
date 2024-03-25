02129d54bcb9c33469ba9021f2b0d8e7e8524145
ashkam58 <ashkam58@gmail.com>
2024-03-25 15:41:23 +0530

    sec tion 35

--- index.js
+++ index.js
@@ -1,19 +1,58 @@
 //SECTION: 33 CREATING SERVERS WITH THE EXPRESS
 
-import express from 'express'
+// import express from 'express'
 
-const app = express();
-const port = 8080;
+// const app = express();
+// const port = 8080;
 
 // app.use((req, res)=>{
 //     console.log("NEW REQUEST IS BEING MADE TO THE SERVER");
 //     res.send('<h1>This is response being send to the server</h1>')
 // })
 
-app.get('/cats', (req, res)=>{
-    console.log("Cat Request")
+// app.get('/cats', (req, res)=>{
+//     console.log("Cat Request");
+// })
+
+// app.get('/dogs', (req, res)=>{
+//     console.log("Dog Request");
+// })
+
+// app.post('/cats', (req, res)=>{
+//     console.log("Cat Request but in post");
+// })
+
+// app.get('/', (req, res)=>{
+//     console.log("This is the home page");
+// })
+
+// app.listen(port, ()=>{
+//     console.log("App is listening to the port 8080");
+// })
+
+
+
+
+
+
+
+
+// SECTION 35 DEFINING RESTFUL ROUTes
+import express from "express";
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/tacos', (req, res)=>{
+    
+    res.send("Get method called");
 })
 
-app.listen(port, ()=>{
-    console.log("App is listening to the port 8080");
+app.post('/tacos',(req, res)=>{
+    const {meat, qty} = req.body;
+    res.send(`here are you ${qty} ${meat} tacos`);
+})
+
+app.listen(3000, ()=>{
+    console.log("App is listening to the port 3000");
 })
